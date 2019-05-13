11424b5a47bf458f312bdb7329db24cc4d9f06f2
Zach Jayson <zachjayson@Zachs-MacBook-Air.local>
2019-05-13 15:17:44 -0700

    styled ejs and fixed it

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var path = require('path');
 var axios = require('axios');
 var ejs = require('ejs');
 
-
+app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
@@ -13,17 +13,21 @@ app.get('/', (req, res) => {
 })
 
 app.get('/search', (req, res) => {
-    var apiUrl = `https://api.edamam.com/search?q=avocado+${req.query.foodType}&app_id=5a6621e2&app_key=a1dab622263e25d6b66bb8b4b18e852f&from=0&to=3&calories=591-722&health=alcohol-free`
+    var apiUrl = `https://api.edamam.com/search?q=avocado+${req.query.foodType}&app_id=5a6621e2&app_key=a1dab622263e25d6b66bb8b4b18e852f&from=0&to=6`
     axios.get(apiUrl)
         .then(function (response) {
-            console.log(response.data)
-           // var html = ejs.render('<%= recipes.join(", "); %>', {recipes: response.data})
-            res.send(html);
+            console.log(response.data.hits[0].recipe)
+            // var html = ejs.render('<%= recipes.join(", "); %>', {recipes: response.data})
+            res.render('pages/results', {
+                recipes: response.data.hits,
+                searchTerm: req.query.foodType
+            });
+            res.send(response.data.hits[0].recipe);
         })
         .catch(function (error) {
             res.send(req.query)
         });
-    
+
 })
 
 
