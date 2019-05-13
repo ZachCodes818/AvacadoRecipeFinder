b889e56c6336fd296e02f64b3d692226ea634e0d
Zach Jayson <zachjayson@Zachs-MacBook-Air.local>
2019-05-13 13:16:28 -0700

    set up heroku files

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 8000;
 var path = require('path');
 var axios = require('axios');
 var ejs = require('ejs');
